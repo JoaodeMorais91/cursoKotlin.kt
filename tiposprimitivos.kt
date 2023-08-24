3aa30d67bd12d3f1e4b9f7652ed9fc8676a4bd2e
JoaodeMorais91 <143114416+JoaodeMorais91@users.noreply.github.com>
2023-08-24 14:07:42 -0300

    Add files via upload

---
+++ tiposprimitivos.kt
@@ -0,0 +1,31 @@
+import java.math.BigDecimal
+
+fun main(){
+    //Byte, Short, Int, Long
+
+    val byte: Byte = 127
+    val short: Short = 32767
+    val int: Int = 2147483647
+    val long: Long = 9223372036854775807
+
+    //float e double
+    val float: Float = 1.38423f
+    val double: Double = 1.38423
+    val bigDecimal: BigDecimal = BigDecimal.ZERO
+
+    //Boolean
+    val boolean: Boolean = false
+    val boolean2: Boolean = true
+
+    //Char
+    val char: Char = 'F'
+
+    //string
+    val string: String = "Joao"
+
+    //Array
+    val array: Array<Int> = arrayOf(0,2,4,6,8,10,12,14,16,18,20)
+
+//  var boolean3 = false
+//  var boolean3 = 5
+}
